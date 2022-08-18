04d12474edee2b8d10890d1b964323427f5748f4
Ather Ahmad <atherahmad49@yahoo.com>
2022-08-18 11:27:48 +0200

    deployment with express

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-import express from "express";
+const express = require("express")
+const dotenv = require("dotenv")
+var path = require('path');
+
+dotenv.config()
+const PORT = process.env.PORT
+
 
 const app = express();
+app.use(express.static(path.join(__dirname ,"build")))
 
-app.listen()
+app.use("/", (req, res) => {
+    res.sendFile(path.join(__dirname , "build", "index.html"))
+})
+
+app.listen(PORT, ()=>console.log(`server running at port ${PORT}`))
